f94de31c3a346330e8674cb6bc7a15702abd1d2e
intfreedom <liuwenjunxjtu@163.com>
2019-03-13 23:01:30 +0800

    insist
    insist

---
+++ Everyday/20190313/getChar/test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+{
+	int c;
+
+	while ((c = getchar()) != EOF)
+		putchar(c);
+}
+
+//main()
+//{
+//	int c;
+//
+//	c = getchar();
+//	while(c != EOF){
+//		putchar(c);
+//		c = getchar();
+//	}
+//}
