bd4a8b7d159c115bca00812e0ba76f67181f1821
intfreedom <liuwenjunxjtu@163.com>
2019-03-19 20:38:57 +0800

    1
    1

--- Everyday/20190316/DemoChange/change.c
+++ Everyday/20190316/DemoChange/change.c
@@ -2,16 +2,11 @@
 
 //int auto(char s[]);
 
-int auto(char s[])
+int atoi(char s[])
 {
 	int i, n;
 	n = 0;
 	for(i=0; s[i] >= '0' && s[i] <= '9'; ++i)
-		n = n*10 + (s[i] - '0');
+		n = 10*n + (s[i] - '0');
 	return n;
-}
-
-int main()
-{
-	printf("%d", auto('12345'));
 }
