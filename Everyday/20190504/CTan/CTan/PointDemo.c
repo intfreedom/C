c46d4cd75728550dffeb61b53aa809c0ffe64f53
intfreedom <liuwenjunxjtu@163.com>
2019-05-04 18:31:39 +0800

    insist 20190504

---
+++ Everyday/20190504/CTan/CTan/PointDemo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+	/*char str[20];
+	str = "i love you!";*/
+	//×Ö·ûÖ¸Õë±äÁ¿£»
+	char *a;
+	a = "I love myself!";
+	printf("%s\n", a);
+	//printf("%s\n", a[0]);
+	//printf("%s\n", a[1]);
+
+	system("pause");
+
+}
