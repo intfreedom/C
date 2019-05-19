cc7e7a150bc7a74212dd02123815249e7f6233d3
intfreedom <liuwenjunxjtu@163.com>
2019-05-19 20:23:20 +0800

    1

---
+++ Everyday/TheCProgrammingLanguage/Chapter4.2/Chapter4.2/atofdemo.c
@@ -0,0 +1,67 @@
+#include<ctype.h>
+#include<stdio.h>
+#define MAXLINE 100
+
+/*atof: convert string s to double*/
+double atof(char s[])
+{
+	double val, power;
+	int i, sign;
+
+	for (i = 0; isspace(s[i]);i++)/*skip white space*/
+	{
+		;
+	}
+	sign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	for (val = 0.0; isdigit(s[i]); i++)
+		val = 10.0*val + (s[i] - '0');
+	if (s[i] == '.')
+		i++;
+	for (power = 1.0; isdigit(s[i]); i++)
+	{
+		val = 10.0*val + (s[i] - '0');
+		power *= 10;
+	}
+	return sign * val / power;
+}
+
+
+int getline(char s[], int lim)
+{
+	int c, i;
+
+	i = 0;
+	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+		s[i++] = c;
+	if (c == '\n')
+		s[i++] = c;
+	s[i] = '\0';
+	return i;
+}
+
+main()
+{
+	double sum, atof(char[]);
+	char line[MAXLINE];
+	int getline(char line[], int max);
+
+	sum = 0;
+	while (getline(line, MAXLINE) > 0)
+		printf("\t%g\n",sum+=atof(line));
+	return 0;
+
+}
+
+void main1()
+{
+	double sum;
+	double atof(char[]);
+	char name[] = "string";
+	sum=atof(name);
+	printf("%f",sum);
+	
+	system("pause");
+}
+
