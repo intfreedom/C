2afa0ce84fadd7cdc3c8e341a58734f88841982c
intfreedom <liuwenjunxjtu@163.com>
2019-05-26 20:48:56 +0800

    insist20190526

---
+++ Everyday/TheCProgrammingLanguage/Chapter6/chapter6.3/chapter6.3/getWord.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+#define MAXWORD 100
+
+int getword(char*, int);
+int binsearch(char*, struct key*, int);
+
+/*conut c key words*/
+main()
+{
+	int n;
+	char word[MAXWORD];
+
+	while (getword(word, MAXWORD) != EOF)
+		if (isalpha(word[0]))
+			if ((n = binsearch(word,keytab,EYS)) >= 0)
+				keytab[n].count++;
+	for (n = 0; n < NKEYS; n++)
+		if (keytab[n].count > 0)
+			printf("%4d %s\n", keytab[n].count, keytab[n].word);
+	return 0;
+}
+
+/*binsearch:find word in tab[0]...tab[n-1]*/
+int binsearch(char *word, struct key tab[], int n)
+{
+	int cond;
+	int low, high, mid;
+
+	low = 0;
+	high = n - 1;
+	while (low <= high) {
+		mid = (low + high) / 2;
+		if ((cond = strcmp(word, tab[mid].word)) < 0)
+			high = mid - 1;
+		else if (cond > 0)
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+}
