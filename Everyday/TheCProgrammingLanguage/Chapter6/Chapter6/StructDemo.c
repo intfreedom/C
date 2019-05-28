4c6d43ed82e574a0168ebde1958a57be7487855b
intfreedom <36918526+intfreedom@users.noreply.github.com>
2019-05-28 17:06:22 +0800

    1

--- Everyday/TheCProgrammingLanguage/Chapter6/Chapter6/StructDemo.c
+++ Everyday/TheCProgrammingLanguage/Chapter6/Chapter6/StructDemo.c
@@ -6,3 +6,24 @@ struct point {
 	int y;
 };
 
+struct rect {
+	struct point pt1;
+	struct point pt2;
+};
+
+/*makepoint: make a point from x and y components*/
+struct point makepoint(int x, int y)
+{
+	struct point temp;
+
+	struct rect screen;
+	struct point middle;
+	struct point makepoint(int, int);
+
+	screen.pt1 = makepoint(0, 0);
+
+	temp.x = x;
+	temp.y = y;
+	return temp;
+}
+
