561bc0e99ca1700dd4e5c018aa7beee135f07539
intfreedom <36918526+intfreedom@users.noreply.github.com>
2019-06-18 17:08:31 +0800

    1

--- Everyday/TheCProgrammingLanguage/Chapter5.3/Chapter5.3/pointVsArray.c
+++ Everyday/TheCProgrammingLanguage/Chapter5.3/Chapter5.3/pointVsArray.c
@@ -17,7 +17,9 @@ void main()
 	printf("%0x\n%0x\n", aa + 1, bb + 1);//1afe9d    fa7b41地址值每次都不一样；
 
 	printf("%0x %0x\n", &aa, &aa+1);//相差aa的字符数+1，表示这个字符串结束了的下一数据；
+	printf("*(&aa+1):\n");
 	printf("%s %s\n", *&aa, *(&aa + 1));//aa字符串内容----乱码；
+	printf("%s why: %s  tooo: %s\n", *&aa,*(&aa + 4),*(&aa+2));//aa字符串内容----乱码；
 	printf("%0x %0x\n", bb, bb + 1);//相差1；
 	printf("%0x %0x\n", &bb, &bb+1);//相差4,表示下一个地址；
 
