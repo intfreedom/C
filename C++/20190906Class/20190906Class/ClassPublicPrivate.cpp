8501d3dfc331e63f267c6f2aaef7e58d0770f368
intfreedom <liuwenjunxjtu@163.com>
2019-09-07 23:51:18 +0800

    1

--- C++/20190906Class/20190906Class/ClassPublicPrivate.cpp
+++ C++/20190906Class/20190906Class/ClassPublicPrivate.cpp
@@ -1,9 +1,34 @@
-#include<string.h>
-#include<stdarg.h>
-#include<stdio.h>
+#include<iostream>
+//#include<cstdio>
+using namespace std;
+/* C++为了解决软件危机；*/
 
-class CFile
+int main()
 {
-	FILE *fp;
-	bool m_bEnBuffer;
+	/*FILE *fp;
+	bool m_bEnBuffer;*/
+
+	//std::cout << "helloworld\n" << std::endl;
+
+	/*char str[50];
+	printf("I love old!!!\n");
+	getchar();
+	return 0;*/
+
+	int a = 10;
+	int &b = a;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+
+	b = 100;
+
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+
+	system("pause");
+
+	return 0;
+
+	
 };
+
